b8d8972c4ab587974a8359637438b7d81db3f3d8
CarlosAsrc <carlos.asrc@gmail.com>
2020-09-22 02:18:24 -0300

    Adds elasticsearch-kafka-consumer base project

---
+++ elasticsearch-kafka-consumer/src/main/java/com/github/carlosasrc/samplekafkaelasticsearch/elasticsearchkafkaconsumer/ElasticsearchKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.carlosasrc.samplekafkaelasticsearch.elasticsearchkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchKafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchKafkaConsumerApplication.class, args);
+    }
+
+}
